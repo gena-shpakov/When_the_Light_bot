d9056aefab8624226f97451a411441c231a31506
gena-shpackov <spakovgennadij@gmail.com>
2026-01-30 14:19:58 +0200

    Big change

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN", "")
+API_ID = os.getenv("API_ID", "")
+API_HASH = os.getenv("API_HASH", "")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "")
+CHANNEL_URL = os.getenv("CHANNEL_URL", "")
